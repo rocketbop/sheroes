3a3eceed1bf779a7ad39a0e561d8bab3364ec79f
monkeywrong <paulgregorybyrne@gmail.com>
2014-03-29 16:10:07 +0000

    Added fixtures.

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :projects # This would have been used anyway because of the configuration, but otherwise you could specify
+  
+  # If I create a project without any attributes passed, the project should be invalid, and there should be the following
+  # errors
+   test "project attributes must not be empty" do
+     project = Project.new
+     assert project.invalid?
+     assert project.errors[:title].any?, "#{:title} should be invalid." #string will be displayed if fails
+     assert project.errors[:description].any?
+     assert project.errors[:headline].any?
+     assert project.errors[:project_type].any?
+   end
+   
+   
 end
