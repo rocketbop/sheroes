4e13a4ef96d60a9d0f3eecc8627c4787d7c49091
monkeywrong <paulgregorybyrne@gmail.com>
2014-03-30 21:27:51 +0100

    Updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Sideheroes::Application.routes.draw do
+  get "store/index"
+  `get "store/index"`
   resources :projects
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'store#index', as: 'store'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
