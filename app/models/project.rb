e458ccb44348bcdc2b28dc62f504cb7c40ab75b2
monkeywrong <paulgregorybyrne@gmail.com>
2014-03-31 20:35:40 +0100

    Added Sass color variables

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-validates :title, :description, :headline, :project_type, presence: true
-validates :title, uniqueness: true  
+  validates :title, :description, :headline, :project_type, presence: true
+  validates :title, uniqueness: true
 
 end
