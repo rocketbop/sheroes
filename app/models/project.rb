abc81e3fc08052087d39fb7cfc8a54b70eac5b6d
monkeywrong <paulgregorybyrne@gmail.com>
2014-04-04 22:02:16 +0100

    Added caching

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,11 @@
 class Project < ActiveRecord::Base
   validates :title, :description, :headline, :project_type, presence: true
   validates :title, uniqueness: true
+  
+  def self.latest
+    Project.order(:updated_at).last
+  end
+  
+  
 
 end
